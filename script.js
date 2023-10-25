d835dd4a0aeb4f3968c5d14711ce1d69d571cbe3
ShubhamGupta7050 <121475393+ShubhamGupta7050@users.noreply.github.com>
2023-10-25 23:23:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+function circleMouseFollower(){
+    window.addEventListener("mousemove",function(dets){
+       document.querySelector("#minicircle").style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
+    })
+}
+circleMouseFollower();
